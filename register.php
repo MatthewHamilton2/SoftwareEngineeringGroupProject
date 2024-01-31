cb9b6d22045fea1c914ac75d4436b9a3442cadce
AengusLennox <157803579+AengusLennox@users.noreply.github.com>
2024-01-31 23:47:49 +0000

    added header and form div

--- register.php
+++ register.php
@@ -7,57 +7,68 @@
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-    <section>
-    <form action="register.php" method="post">
-        <input type = "text" name="username" placeholder="Username"><br>
-        <input type = "password" name="password" placeholder="Password"><br>
-        <input type = "text" name="email" placeholder="Email"><br>
-        <label>Are you a student or Educator?</label><br>
-        <select name="usertype">
-            <option value="student">Student</option>
-            <option value="educator">Educator</option>
-        </select>
-        <br>
-        <input type = "submit" name = "submit" value = "Register">
-    </form>
-        <?php
-        include("connect.php");
-        if(isset($_POST['submit'])){
-        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
-        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-        $type = $_POST["usertype"];
+    <header>
+		<div class="logo">
+		  <img src="collabNexusLogo.png" alt="Collab Nexus Logo">
+		  <h1>Task Troopers: Login</h1>
+		</div>
+		<div class="profile">
+		  <img src="placeholder.jpg" alt="Profile Picture" class="profile-picture">
+		</div>
+	</header>
+	<section>
+	<div class="form">
+		<form action="register.php" method="post">
+			<input type = "text" name="username" placeholder="Username"><br>
+			<input type = "password" name="password" placeholder="Password"><br>
+			<input type = "text" name="email" placeholder="Email"><br>
+			<label>Are you a student or Educator?</label><br>
+			<select name="usertype">
+				<option value="student">Student</option>
+				<option value="educator">Educator</option>
+			</select>
+			<br>
+			<input type = "submit" name = "submit" value = "Register">
+		</form>
+			<?php
+			include("connect.php");
+			if(isset($_POST['submit'])){
+			$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+			$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+			$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+			$type = $_POST["usertype"];
 
-        if(empty($username)){
-            echo"Please enter a Username"."<br>";
-        }
-        else if(empty($password)){
-            echo"Please enter a Password"."<br>";
-        }
-        else if(empty($email)){
-            echo"Please enter an Email Address"."<br>";
-        }
-        else {
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-        try{
-        $sql = "INSERT INTO users (username, password, email, type) VALUES ('$username', '$hashedPassword', '$email', '$type')";
-        mysqli_query($conn, $sql);
-        }
-        catch(mysqli_sql_exception){
-            $sql = "SELECT username FROM users WHERE username = '$username'";
-            $result = mysqli_query($conn, $sql);
-            if(mysqli_num_rows($result) > 0){
-                echo"That Username is already taken";
-            }
-            else{
-                echo"That Email Address is already taken";
-            }
-        }
-        mysqli_close($conn);
-        }
-        }
-        ?>
-    <h1>Already have an account? Then log in <a href="login.php">here</a></h1>
+			if(empty($username)){
+				echo"Please enter a Username"."<br>";
+			}
+			else if(empty($password)){
+				echo"Please enter a Password"."<br>";
+			}
+			else if(empty($email)){
+				echo"Please enter an Email Address"."<br>";
+			}
+			else {
+			$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+			try{
+			$sql = "INSERT INTO users (username, password, email, type) VALUES ('$username', '$hashedPassword', '$email', '$type')";
+			mysqli_query($conn, $sql);
+			}
+			catch(mysqli_sql_exception){
+				$sql = "SELECT username FROM users WHERE username = '$username'";
+				$result = mysqli_query($conn, $sql);
+				if(mysqli_num_rows($result) > 0){
+					echo"That Username is already taken";
+				}
+				else{
+					echo"That Email Address is already taken";
+				}
+			}
+			mysqli_close($conn);
+			}
+			}
+			?>
+		<h1 class="alternative">Already have an account? Then log in <a href="login.php">here</a></h1>
+	</div>
     </Section>
 </body>
 </html>
