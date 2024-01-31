b286e9549b02d8e9ccfff87612213609d8c7a486
AengusLennox <157803579+AengusLennox@users.noreply.github.com>
2024-01-31 23:43:02 +0000

    Add files via upload

--- login.php
+++ login.php
@@ -7,44 +7,57 @@
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-    <Section>
-    <form action="login.php" method="post">
-        <input type = "text" name="username" placeholder="Enter Username or Email"><br>
-        <input type = "password" name="password" placeholder="Enter Password"><br>
-        <input type = "submit" name = "submit" value = "Log in">
-    </form>
-        <?php
-        include("connect.php");
-        if(isset($_POST['submit'])){
-        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+    <header>
+		<div class="logo">
+		  <img src="collabNexusLogo.png" alt="Collab Nexus Logo">
+		  <h1>Task Troopers: Login</h1>
+		</div>
+		<div class="profile">
+		  <img src="placeholder.jpg" alt="Profile Picture" class="profile-picture">
+		</div>
+  </header>
+	<Section>
+		<div class="form">
+			<form action="login.php" method="post">
+				<h1>Username or Email</h1>
+				<input type = "text" name="username" placeholder="Enter Username or Email"><br>
+				<h1>Password</h1>
+				<input type = "password" name="password" placeholder="Enter Password"><br>
+				<input type = "submit" name = "submit" value = "Log in">
+			</form>
+				<?php
+				include("connect.php");
+				if(isset($_POST['submit'])){
+				$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+				$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
 
-        if(empty($username)){
-            echo"Please enter a Username or Email Address"."<br>";
-        }
-        else if(empty($password)){
-            echo"Please enter a Password"."<br>";
-        }
-        else {
+				if(empty($username)){
+					echo"Please enter a Username or Email Address"."<br>";
+				}
+				else if(empty($password)){
+					echo"Please enter a Password"."<br>";
+				}
+				else {
 
-        $sql = "SELECT password FROM users WHERE (username = '$username' OR email = '$username')";
-        $result = mysqli_query($conn, $sql);
-        if(mysqli_num_rows($result) > 0){
-            if(password_verify($password, mysqli_fetch_assoc($result)["password"])){
-                echo"login successful";
-            }
-            else{
-                echo"That Username or Password was incorrect";
-            }
-        }
-        else{
-            echo"That Username or Password was incorrect";
-        }
-        mysqli_close($conn);
-        }
-        }
-        ?>
-    <h1>Dont have an account? Then register <a href="register.php">here</a></h1>
+				$sql = "SELECT password FROM users WHERE (username = '$username' OR email = '$username')";
+				$result = mysqli_query($conn, $sql);
+				if(mysqli_num_rows($result) > 0){
+					if(password_verify($password, mysqli_fetch_assoc($result)["password"])){
+						echo"login successful";
+					}
+					else{
+						echo"That Username or Password was incorrect";
+					}
+				}
+				else{
+					echo"That Username or Password was incorrect";
+				}
+				mysqli_close($conn);
+				}
+				}
+				?>
+			<h1 class="register">Dont have an account? Then register <a href="register.php">here</a></h1>
+		</div>
     </Section>
 </body>
 </html>
