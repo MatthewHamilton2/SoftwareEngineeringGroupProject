9088a0ed410d8f0ebf85a251b1d191c2d1775665
AlexanderRaez <91124825+AlexanderRaez@users.noreply.github.com>
2024-03-12 12:20:18 +0000

    Update EducatorGroups.php

--- EducatorGroups.php
+++ EducatorGroups.php
@@ -11,10 +11,54 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>student-group-page</title>
   <link rel="stylesheet" href="style.css">
+  <style>
+
+.dropbtn {
+  background-color: #3498DB;
+  color: white;
+  padding: 15px;
+  font-size: 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-left: auto;
+}
+
+.dropbtn:hover, .dropbtn:focus {
+  background-color: #2980B9;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  width: 100%;
+  overflow: auto;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown a:hover {background-color: #ddd;}
+
+.show {display: block;}
+</style>
+  
 </head>
 <body id="studentGroupBody">
-  <div id="nav-bar">
-    <button class="backButton" onclick="goto('index.php')">&larr;</button>
+  <div id="nav-bar" style="width: 150px">
+    <button class="backButton" onclick="goto('index.php')">&larr; Home</button>
     <div id="nav-bar-header">
     <?php
         $groupid = $_GET['groupid'];
@@ -23,43 +67,47 @@
         $groupname = mysqli_fetch_assoc($result)['groupname'];
         echo "<h3 style='text-align:center; font-size: 20px;'>".$groupname."</h3>";
       ?>
-      <button id="settings-button">Settings</button>
+     
     </div>
 
-    <div id="channels">
-      <h4>Discussions</h4>
+    <div id="channels" style="background: white;">
+      <h4 style='margin-bottom: 5px'>Discussions</h4>
       <?php
           $sql = "SELECT discussionName FROM discussions where groupid = '$groupid'";
           $result = mysqli_query($conn, $sql);
           while($row = mysqli_fetch_assoc($result)){
               $channelName = $row['discussionName'];
               echo "
-    <button onclick=\"goto('EducatorGroups.php?groupid=".$groupid."&channel=".$channelName."')\">$channelName</button><br>
+    <button onclick=\"goto('EducatorGroups.php?groupid=".$groupid."&channel=".$channelName."')\" style='background: none; font-size: 20px; color: black; text-decoration: underline;'>$channelName</button><br>
 ";
 
           }
       ?>
 
       <div id="groupheader" class="groupdisplay">
-				<span style="font-size:30px;">
-          <a href="javascript:void(0)" class="addgroup" onclick="appearModal('modalDiscussion')">Create Discussion</a>
-				</span>
+				<button style="font-size:18px; background-color: lightgrey; width: 100%; margin-left: 0; padding:5px; margin-bottom: 0px">
+          <b href="javascript:void(0)" class="addgroup" onclick="appearModal('modalDiscussion')" style='cursor:pointer; color:black;'>Create Discussion</b>
+				</button>
 			</div>
-
-
-      <?php
-          $sql = "SELECT joincode FROM educatorgroup WHERE groupid = '$groupid'";
-          $result = mysqli_query($conn, $sql);
-          $code = mysqli_fetch_assoc($result)['joincode'];
-          echo"
-          <h4> Join Code: </h4>
-          <h3 style= 'margin-left:15px'>
-          ".$code."
-          </h3>";
-      ?>
+      
     </div>
-
     
+
+
+    <div class="dropdown">
+  <button onclick="toggleDropdown()" class="dropbtn">More options</button>
+  <div id="myDropdown" class="dropdown-content">
+   
+          
+    <button onclick="openForm('popupForm2')" style='font-size: 15px'>Create Student Group</button>
+    <button onclick="openForm('popupForm1')" style='font-size: 15px; width: 93%;'>Add Student</button>
+    <button onclick="openForm('popupForm')" style='font-size: 15px'>Create Student Account</button>
+
+  </div>
+</div>
+
+
+
   </div>
   
 
@@ -74,7 +122,7 @@
       <?php
       $groupid = $_GET['groupid'];
       $channel = $_GET['channel'];
-        $sql = "SELECT * FROM (SELECT * FROM discussionmessage WHERE (groupid = ".$groupid." AND discussionName = '".$channel."') ORDER BY timeSent DESC) AS subquery ORDER BY subquery.timeSent ASC";
+        $sql = "SELECT * FROM (SELECT * FROM discussionmessage WHERE (groupid = ".$groupid." AND discussionName = '".$channel."') ORDER BY timeSent DESC LIMIT 10) AS subquery ORDER BY subquery.timeSent ASC";
         $result = mysqli_query($conn, $sql);
         echo"<div class = 'chatcontainer'>";
         while($row = mysqli_fetch_assoc($result)){
@@ -97,14 +145,63 @@
     </div>
   </div>
   <div id="members-bar">
-    <div id="members">
-      <h3>Educators</h3>
+
+
+
+
+
+      <?php
+          $sql = "SELECT joincode FROM educatorgroup WHERE groupid = '$groupid'";
+          $result = mysqli_query($conn, $sql);
+          $code = mysqli_fetch_assoc($result)['joincode'];
+          echo"
+          <h4 style= 'margin-top:0px'> Join Code: </h4>
+          <h3 style= 'font-size: 15px; padding: 10px; margin-left:0px; background-color: white; color: black; width: 100%; margin-top: 0px'>
+          ".$code."
+          </h3>";
+      ?>
+
+
+
+    <div id="members" style="background: #48a1f8; width: 100%; margin-left: 0; color: white;">
+      <h3 style= "margin-left: 28%; font-size: 20px;">Educators</h3>
     </div>
     <div id="members-list">
       <ul>
 
       </ul>
     </div>
+
+    <button type="button" onclick="appearModal('modalAnnouncement')" style="font-size: 20px; width:100%; margin-left: 0; margin-bottom: 0px"> Make New Announcement </button>
+
+					
+	
+					
+		<h3 style='text-align:center; color:black; background-color: white; padding: 10px; margin-top: 0px; text-decoration: none;'>Recent announcements</h3>
+				
+				
+			<div id="annbody" class="announcements" style="display: none; background-color: white">
+				<?php
+				$groupid = $_GET['groupid'];
+				$sql = "SELECT announcementtext, sender, timesent FROM announcements WHERE groupid = '$groupid' ORDER BY timesent DESC LIMIT 3";
+				$result = mysqli_query($conn, $sql);
+				while ($row = mysqli_fetch_assoc($result)) {
+					$text = $row['announcementtext'];
+					$displayedtext = substr($text, 0, 150) . "...";
+					$sender = $row['sender'];
+					$time = $row['timesent'];
+					echo "
+					<div class='announcement'>
+					<p>" . $sender . "<br>" . $displayedtext . "<br>" . $time . "</p>
+					</div>
+					";
+				}
+				?>
+			</div>
+		</section>
+
+    
+
   </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <?php
@@ -121,76 +218,102 @@
   "
   ?>
 
-<div>
-<div class="popup" id="popupForm2" style="display: none;">
-		<h15 style="margin-bottom: 10px; font-size: 20px; padding: 5px ">Create student group</h15>
-		<form method="post" id="groupform" action="createGroupEducator.php">
-		<?php
-			$sql = "SELECT user FROM educatorgroups2users WHERE groupid = ".$groupid;
-			$result = mysqli_query($conn, $sql);
-			echo"<form method='post' id='groupform' action='createGroupEducator.php'>";
-			while($row = mysqli_fetch_assoc($result)){
-				$name = $row["user"];
-				if(!($name == $_SESSION['username'])){
-				echo"
-				<input type='checkbox' name='user[]' value='".$name."' id='".$name."'>
-				<label for='".$name."'>".$name."</label><br>
-			";
-			}
-		}
-		?>
-		<input type='text' name='groupName' placeholder='Group Name'>
-		<input type='submit' name='groupcreate' value='creategroup'>
-		</form>
-	<button onclick="closeForm('popupForm2')" style="background: azure;color: black;color: black; font-size: 20px; position: absolute; right: 10px; top: 10px;">Close</button>
-  </div>
-  </div>
 
-  <div>
+
+      <div class="popup" id="popupForm2" style="display: none;">
+		    <h15 style="margin-bottom: 10px; font-size: 20px; padding: 15px ">Create student group</h15>
+       
+        <input type='text' name='groupName' placeholder='Group Name' style="font-size: 15px; padding: 5px; margin-top: 10px">
+        <form method="post" id="groupform" action="createGroupEducator.php" style="font-size: 18px; padding: 10px;">
+		    <?php
+			    $sql = "SELECT user FROM educatorgroups2users WHERE groupid = ".$groupid;
+			    $result = mysqli_query($conn, $sql);
+			    echo"<form method='post' id='groupform' action='createGroupEducator.php'>";
+			    while($row = mysqli_fetch_assoc($result)){
+			  	  $name = $row["user"];
+				    if(!($name == $_SESSION['username'])){
+				    echo"
+				    <input type='checkbox' name='user[]' value='".$name."' id='".$name."'>
+				    <label for='".$name."'>".$name."</label><br>
+			    ";
+          }
+          }
+		    ?>
+		    
+		    <input type='submit' name='groupcreate' value='Create Group' style="width: auto;">
+		    </form>
+	      <button onclick="closeForm('popupForm2')" style="background: azure;color: black;color: black; font-size: 20px; position: absolute; right: 10px; top: 10px;">Close</button>
+      </div>
+   
+
+      
 					
 						<div class="popup" id="popupForm1" style="display: none;">
-							<form method="post" style="border: 5px solid red" id="adduser">
+							<form method="post"  id="adduser">
  							<h1 style="margin-bottom: 10px; font-size: 20px; padding: 5px ">Enter student username</h1>
 							<input type="text" name = "studentusername" placeholder="Username">
 							</form>
 						<button onclick="closeForm('popupForm1')" style="background: azure;color: black;color: black; font-size: 20px; position: absolute; right: 10px; top: 10px;">Close</button>
 					</div>
 
-</div>
+      
 
-<div>
+          
 						
 							<div class="popup" id="popupForm" style="display:none">
-								<h14>Create Student Account</h14>
-									<form method="post" style="border: 5px solid black" id="accountCreate">
-									<h1 style="font-size: 20px; padding-left: 5px;">Enter username</h1>
+								<h14 style="font-size: 20px;">Create Student Account</h14>
+									<form method="post"  id="accountCreate">
+									<h1 style="font-size: 15px; padding-left: 5px;">Enter username</h1>
 									<input type = "text" name="username" placeholder="Username"><br>
-									<h1 style="font-size: 20px; padding-left: 5px;">Enter password</h1>
+									<h1 style="font-size: 15px; padding-left: 5px;">Enter password</h1>
 									<input type = "password" name="password" placeholder="Password"><br>
-									<h1 style="font-size: 20px; padding-left: 5px;">Enter email</h1>
+									<h1 style="font-size: 15px; padding-left: 5px;">Enter email</h1>
 									<input type = "text" name="email" placeholder="Email"><br>
 									<input type = "submit" name = "submit" value = "Register">
-								</form>
-							<button onclick="closeForm('popupForm')" style="background: azure;color: black;color: black; font-size: 20px; position: absolute; right: 10px; top: 10px;">Close</button>
-						</div>
-					</div>
+								  </form>
+							  <button onclick="closeForm('popupForm')" style="background: azure;color: black;color: black; font-size: 20px; position: absolute; right: 10px; top: 10px;">Close</button>
+					  	</div>
+				
 
-          <dialog id="modalAnnouncement">
-		<button onclick="disappearModal('modalAnnouncement')">X</button><br>
-		<?php
-		$groupid = $_GET['groupid'];
-		echo "
-            <Form id='createAnnouncement' method='post'>
-				<input type = 'text' placeholder = 'Announcement' id='announcementBox' name='announcementText' maxnumber='100'>
-				<input type='submit'>
-			</Form>
-			"
-		?>
-	</dialog>
+      <dialog id="modalAnnouncement">
+		    <button onclick="disappearModal('modalAnnouncement')" style="background-color: azure; color: black; font-size: 20px; right: 10px">Close</button><br>
+		    <?php
+		    $groupid = $_GET['groupid'];
+		    echo "
+             <Form id='createAnnouncement' method='post'>
+				    <input type = 'text' placeholder = 'Announcement' id='announcementBox' name='announcementText' maxnumber='100'>
+				    <input type='submit'>
+			    </Form>
+			    "
+		    ?>
+	    </dialog>
+
 
 
   
-  <script>
+<script>
+
+
+
+
+  function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle("show");
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
     function goto(destination){
         location.href = destination;
     }
@@ -354,15 +477,7 @@ $(document).ready(function(){
 
   <section>
 			<div id="annheader" class="announcements">
-				<span>
-				 <button id="CreateStudentAccountButton" onclick="openForm('popupForm')">Create Student Account</button>	<button id ="addStudentToGroup" onclick="openForm('popupForm1')">Add Student</button><button id="createStudnetGroup" onclick="openForm('popupForm2')">Create student group</button><button type="button" onclick="appearModal('modalAnnouncement')"> Make New Announcement </button>
-
-					
-	
-					
-					<h3>Recent announcements</h3>
-				
-				</span>
+			
 			</div>
 			<div id="annbody" class="announcements" style="display: none">
 				<?php
